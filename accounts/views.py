fbd495f9bec716b5dabaf82b5ad818a1e547174d
rahatahmedchowdhuryrac@gmail.com <rahatahmedchowdhuryrac@gmail.com>
2021-09-12 23:54:33 +0600

    login and registeration added

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 def register(request):
-    pass
+    
+    if request.method == "POST" :
+
+        email = request.POST.get('email')
+
+        username = request.POST.get('username')
+
+        password = request.POST.get('password')
+
+        user = User.objects.create_user(username, email, password)
+
+        user.save()
+
+        return redirect("/")
+
+    return render(request, "register.html")
+
 
 
 def login(request):
-    pass
+
+    if request.method == "POST":
+
+        username = request.POST.get('username')
+
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+
+            return redirect("/")
+
+        else:
+            
+            return render(request, "login.html")
+
+
+    return render(request, "login.html")
 
 
 def logout(request):
