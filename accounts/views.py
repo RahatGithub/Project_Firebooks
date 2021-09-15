3152fe86ba1ae6cffb4fe59e602c3ac09923dc51
rahatahmedchowdhuryrac@gmail.com <rahatahmedchowdhuryrac@gmail.com>
2021-09-15 10:36:30 +0600

    Username is showing when registered

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,54 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 def register(request):
     
     if request.method == "POST" :
 
-        username = request.POST['username']
+        firstname = request.POST['f_name']
+
+        lastname = request.POST['l_name']
 
         email = request.POST['email']
 
+        username = request.POST['username']
+
         password = request.POST['password']
 
-        user = User.objects.create_user(username, email, password)
+        confirm_password = request.POST['confirm_password']
 
-        user.save()
+        if password == confirm_password : 
 
-        return redirect("/")
+            if User.objects.filter(username = username).exists():
+
+                messages.info(request, "Username already taken")
+
+                return redirect("register")
+            
+            elif User.objects.filter(email = email).exists():
+
+                messages.info(request, "Email already in use")
+
+                return redirect("register")
+
+            else :
+
+                user = User.objects.create_user(username, email, password)
+
+                user.first_name, user.last_name = firstname, lastname
+
+                user.save()
+
+                auth.login(request, user)
+
+                return redirect("/")
+        
+        else :
+
+            messages.info(request, 'Password not matched!')
+
+            return redirect("register")
 
     return render(request, "register.html")
 
